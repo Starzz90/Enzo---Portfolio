931343e63d3a063d86b819b7fd06b8d237a3ec5d
StarNanami <enzodtjen@gmail.com>
2026-05-27 11:23:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+var achieve = document.getElementById("ll-ach");
+if (achieve) {
+    achieve.addEventListener("click", function(e){
+        window.location.href = "achievements.html";
+
+    })
+};
+var mini = document.getElementById("MINI()");
+if(mini){
+    mini.addEventListener("click", function(e){
+        window.location.href =  "";
+    })
+};
+var Ach = document.getElementById("Ach");
+if (Ach){
+    Ach.addEventListener("click", function(e){
+        window.location.href = "https://starzz90.github.io/EnzoSite";
+    })
+};
+var Code = document.getElementById("ll-code");
+if(Code){
+    Code.addEventListener("click", function(e){
+        window.location.href = "coding.html";
+    })
+};
+var home = document.getElementById("home");
+if(home){
+    home.addEventListener("click", function(e){
+        window.location.href = "index.html";
+    })
+};
+var Code = document.getElementById("ll-code-t");
+if(Code){
+    Code.addEventListener("click", function(e){
+        window.location.href = "coding.html";
+    })
+};
+function Retur() {
+    window.location.href = "index.html";
+};
+function Code(){
+    window.location.href = "coding.html";
+};

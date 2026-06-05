90c9518d8757e1ab6134d7dd3eacdabed068ac66
Starzz90 <enzodtjen@gmail.com>
2026-06-04 23:50:44 -0500

    testing real

--- connect.php
+++ connect.php
@@ -9,4 +9,29 @@
     if(!$connect){
         die("Koneksi gagal". mysqli_connect_error());
     }
+    $name = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+    $feed = filter_input(INPUT_POST, 'feedback', FILTER_SANITIZE_STRING);
+    $rate = filter_input(INPUT_POST, 'rating', FILTER_SANITIZE_STRING);
+
+    $connect = new mysqli($host, $user, $password, $database);
+    if ($connect->connect_error) {
+        die("Koneksi gagal: " . $connect->connect_error);
+    } else {
+        $stmt = $connect->prepare("INSERT INTO `feedback` (username, feedback, rating) VALUES (?, ?, ?)");
+        $stmt->bind_param("ssis", $name, $feed, $rate);
+        $stmt->execute();
+    }  
+    if ($stmt === false){
+        die('Prepare failed:' . $connect->error);
+    }
+    $stmt->bind_param("sss", $name, $feed, $rate);
+
+    if($stmt->execute()){
+        echo "Your feedback has been recorded";
+    } else {
+        echo "error;" . $stmt->error;
+    }
+
+    $stmt->close();
+    $conn->close();
 ?>
